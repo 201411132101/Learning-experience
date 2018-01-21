6519cf0c74d2124140f259d3c898b72f9e4eb4b2
201411132101 <291817591@qq.com>
2018-01-21 18:53:20 +0800

    更新

---
+++ js/footer.js
@@ -0,0 +1,14 @@
+// 获取访问次数并修改
+var Record = Bmob.Object.extend("Record");
+var query = new Bmob.Query(Record);
+query.first({
+    success: function(object) {
+        // 查询成功
+        object.set("total_times", object.get("total_times")+1);
+        object.save();
+        $("#total_times").text("访问次数: " + object.get("total_times"));
+    },
+    error: function(error) {
+        alert("查询失败: " + error.code + " " + error.message);
+    }
+});
